aff58fb06be139ae03112cd85a74828f650e57e0
ooeygooeygold <amichaelj1@gmail.com>
2018-10-03 18:04:55 -0400

    html css start

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <main>
+        <div>
+          <h1>Things I like</h1>
+          <h2>A Photo Gallery By Michael Yenny</h2>
+        </div>
+        <section>
+          <div className="column">
+            <div className="card-content">
+              <h1>Panda Bears</h1>
+              <p>
+                Pandas are bears native to south-central China, and are
+                objectively the cutest animals on earth."
+              </p>
+            </div>
+            <img src="https://cdn-images-1.medium.com/max/1600/1*i1vVm3EqqDIkyucD0079wg.jpeg" />
+          </div>
+          <div className="column">
+            <div className="card-content">
+              <h1>Miniature Painting</h1>
+              <p>
+                I enjoy painting miniatures. I've only been painting for about
+                6-months, here's some of my work."
+              </p>
+            </div>
+            <img src="https://instagram.ftpf1-1.fna.fbcdn.net/vp/ac13de8676b9e7d7b3a4f1aaf38a9a55/5C3728A2/t51.2885-15/e35/32203464_178578342778306_8009127367152762880_n.jpg" />
+          </div>
+        </section>
+      </main>
+    )
   }
 }
 
-export default App;
+export default App
